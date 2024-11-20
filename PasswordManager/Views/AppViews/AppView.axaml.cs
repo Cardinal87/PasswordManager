4ab2ef72e5c1f6947bef622bd70e3c64a79e9710
Cardinal87 <artem.cheglakov.234@gmail.com>
2024-11-20 21:44:09 +0300

    Added AppView

---
+++ PasswordManager/Views/AppViews/AppView.axaml.cs
@@ -0,0 +1,21 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+
+namespace PasswordManager;
+
+public partial class AppView : UserControl
+{
+    public AppView()
+    {
+        InitializeComponent();
+    }
+    public void EnterPressed(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+
+        }
+    }
+}
