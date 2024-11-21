fffdef805a4b627be5a09ff333886d2e59ec8113
Cardinal87 <artem.cheglakov.234@gmail.com>
2024-11-21 21:38:07 +0300

    added several ThemePatternsfor buttons and seleral StreamGeometry in Icons.axaml

--- PasswordManager/Views/AppViews/AppView.axaml.cs
+++ PasswordManager/Views/AppViews/AppView.axaml.cs
@@ -3,7 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
-namespace PasswordManager;
+namespace PasswordManager.Views.AppViews;
 
 public partial class AppView : UserControl
 {
